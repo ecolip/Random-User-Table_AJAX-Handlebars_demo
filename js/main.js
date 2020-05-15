4fa065ffa7970df0159156485ba81fa61b3d3812
Emma Cheng <ecolipp@gmail.com>
2020-05-15 09:43:41 +0800

    random user table by AJAX+ handlebars

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$.ajax({
+		url: 'https://randomuser.me/api/',
+		data:{results: 3},
+		dataType: 'json',
+		success: function(data) {
+		//     console.log(typeof(data));
+		//     console.log(data);
+			
+			var result = data.results;
+		//   console.log(result);
+			$.each(result, function(index, user){
+			var name = user.name.title+' '+user.name.first+' '+user.name.last;
+			var age = user.dob.age;
+			var phone = user.phone;
+			var email = user.email;
+			
+			
+			var source = $('#table-template').html();
+			var template = Handlebars.compile(source);
+			var context = {index: index+1, name: name, age: age, phone: phone, email: email};
+			var html = template(context);
+
+			$('.table').find('tbody').append(html);
+			}); 
+		}
+	});
+});
